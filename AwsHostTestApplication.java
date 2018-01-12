eb44a10bfeb9327ae3a454535b472bbadb53ad8f
Utkarsh Upadhyay <utkarsh.upadhyay241@gmail.com>
2018-01-12 16:43:07 +0530

    Spring app starter

---
+++ AwsHostTestApplication.java
@@ -0,0 +1,12 @@
+package com.siemens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsHostTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsHostTestApplication.class, args);
+	}
+}
